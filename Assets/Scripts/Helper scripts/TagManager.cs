30fcac6e1883c08dd4854b4e1b337f8e6673e327
Trada Kevin <tradakevin0099@gmail.com>
2023-06-09 16:36:46 +0530

    Add Player Animation

---
+++ Assets/Scripts/Helper scripts/TagManager.cs
@@ -0,0 +1,41 @@
+public static class TagManager
+{
+
+    public static string JUMP_ANIMATION_PARAMETER = "Jump";
+    public static string ATTACK_ANIMATION_PARAMETER = "Attack";
+    public static string DEATH_ANIMATION_PARAMETER = "Death";
+    public static string RUN_ANIMATION_PARAMETER = "Run";
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
