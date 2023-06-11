c487c5a11478bb0f682e03df2cd4691aec438cf2
Trada Kevin <tradakevin0099@gmail.com>
2023-06-11 14:08:07 +0530

    Add BackGround and Ground

--- Assets/Scripts/Helper scripts/TagManager.cs
+++ Assets/Scripts/Helper scripts/TagManager.cs
@@ -8,6 +8,38 @@ public static class TagManager
 
 
     public static string PLAYER_TAG = "Player";
+    public static string GROUND_TAG = "Ground";
+    public static string BACKBROUND_TAG = "BackGround";
+    public static string ENEMY_TAG = "Enemy";
+    public static string OBSTACLE_TAG = "Obstacle";
+    public static string HEALTH_TAG = "Health";
+    public static string GAMEPLAY_CONTROLLER_TAG = "GameplayController";
+
+
+    public static string JUMP_BUTTON = "Jump";
+
+    // animation name
+    public static string JUMP_ANIMATION_NAME = "Jumping";
+    public static string ATTACK_ANIMATION_NAME = "Attack";
+    public static string DEATH_ANIMATION_NAME = "Dead";
+    public static string RUN_ANIMATION_NAME = "Running";
+
+    //scene name
+    public static string GAMEPLAY_SCENE_NAME = "Gameplay";
+    public static string MAIN_MENU_SCENE_NAME = "MainMenu";
+
+    // character names (in gameplay scene)
+    public static string FIRST_PLAYER = "Player1";
+    public static string SECOND_PLAYER = "Player2";
+    public static string THIRD_PLAYER = "Player3";
+
+    //Game data
+    public static string CHARACTER_DATA = "Character";
+    public static string SELECTED_CHARACTER_DATA = "Selected Character";
+    public static string HIGHSCORE_DATA = "Highscore";
+
+    // 0 data not initialized, 1 data initialized
+    public static string DATA_INITIALIZED = "Data Initialized";
 
 
 }
